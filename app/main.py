6762cae29a0738ade5e94ab03c8054619fdfe569
farruhbek devoloper <farruhjohonboyev9@gmail.com>
2026-05-19 14:18:32 +0500

    Add chat router to main application

--- app/main.py
+++ app/main.py
@@ -34,6 +34,7 @@ Base.metadata.create_all(bind=engine)
 # routers
 app.include_router(auth_router, prefix="/auth")
 app.include_router(posts_router)
+app.include_router(chat_router)
 app.include_router(likes_router)
 app.include_router(comments_router)
 app.include_router(users_router)
